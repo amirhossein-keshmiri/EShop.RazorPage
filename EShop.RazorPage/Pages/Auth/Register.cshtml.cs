1e1a3b19733728933ece2bbe8b83a73cc04dffb1
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2023-12-26 21:12:04 +0330

    Implement Register Page

--- EShop.RazorPage/Pages/Auth/Register.cshtml.cs
+++ EShop.RazorPage/Pages/Auth/Register.cshtml.cs
@@ -1,12 +1,56 @@
+﻿using EShop.RazorPage.Models.Auth;
+using EShop.RazorPage.Services.Auth;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace EShop.RazorPage.Pages.Auth
 {
+    [BindProperties]
+    [ValidateAntiForgeryToken]
     public class RegisterModel : PageModel
     {
+        private IAuthService _authService;
+
+        public RegisterModel(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [Display(Name = "شماره تلفن")]
+        [Required(ErrorMessage = "{0} را وارد کنید")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "{0} را وارد کنید")]
+        [MinLength(5, ErrorMessage = "کلمه عبور باید بزرگتر ار 5 کاراکتر باشد")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "تکرار کلمه عبور")]
+        [Required(ErrorMessage = "{0} را وارد کنید")]
+        [Compare("Password", ErrorMessage = "کلمه های عبورر یکسان نیستند")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var result = await _authService.Register(new RegisterCommand()
+            {
+                PhoneNumber = PhoneNumber,
+                Password = Password,
+                ConfirmPassword = ConfirmPassword
+            });
+            if(result.IsSuccess == false)
+            {
+                ModelState.AddModelError(nameof(PhoneNumber), result.MetaData.Message);
+                return Page();
+            }
+            return RedirectToPage("Login");
         }
     }
 }
