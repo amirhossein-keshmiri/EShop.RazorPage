c4b875959f21f3deecb80783359e26849c5b1a0e
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2023-12-26 19:46:06 +0330

    Add Template & Change Layout

---
+++ EShop.RazorPage/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EShop.RazorPage.Pages.Auth
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- EShop.RazorPage/Program.cs
+++ EShop.RazorPage/Program.cs
@@ -3,7 +3,8 @@ using EShop.RazorPage.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages()
+    .AddRazorRuntimeCompilation();
 
 builder.Services.RegisterApiServices();
 builder.Services.AddHttpContextAccessor();
