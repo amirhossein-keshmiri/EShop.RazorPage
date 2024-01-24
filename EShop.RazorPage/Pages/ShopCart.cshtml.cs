e14ac377480adc81e922e375070b822c84fec32b
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2024-01-24 14:38:13 +0330

    Implement ShopCart Page - Part01 (Add To DataBase)

---
+++ EShop.RazorPage/Pages/ShopCart.cshtml.cs
@@ -0,0 +1,66 @@
+using EShop.RazorPage.Infrastructure;
+using EShop.RazorPage.Infrastructure.RazorUtils;
+using EShop.RazorPage.Models;
+using EShop.RazorPage.Models.Orders;
+using EShop.RazorPage.Models.Orders.Command;
+using EShop.RazorPage.Services.Orders;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EShop.RazorPage.Pages;
+public class ShopCartModel : BaseRazorPage
+{
+    private readonly IOrderService _orderService;
+
+    public ShopCartModel(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    public OrderDto? OrderDto { get; set; }
+    public async Task OnGet()
+    {
+        if (User.Identity.IsAuthenticated)
+        {
+            OrderDto = await _orderService.GetCurrentOrder();
+        }
+        else
+        {
+            //cookie
+        }
+    }
+
+    public async Task<IActionResult> OnPostDeleteItem(long id)
+    {
+        if (User.Identity.IsAuthenticated)
+        {
+            return await AjaxTryCatch(() => _orderService.DeleteOrderItem(new DeleteOrderItemCommand()
+            {
+                OrderItemId = id
+            }));
+        }
+        else
+        {
+            //cookie
+            return Page();
+        }
+    }
+    public async Task<IActionResult> OnPostAddItem(long inventoryId, int count)
+    {
+        if (User.Identity.IsAuthenticated)
+        {
+            return await AjaxTryCatch(() => _orderService.AddOrderItem(new AddOrderItemCommand()
+            {
+                UserId = User.GetUserId(),
+                InventoryId = inventoryId,
+                Count = count
+            }));
+        }
+        else
+        {
+            //cookie
+            return Page();
+        }
+    }
+}
+
