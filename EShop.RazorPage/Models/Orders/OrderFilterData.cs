bc5a0a28cbc06566ed942272535d47ebbdb2f43b
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2024-01-29 22:49:53 +0330

    Implement Admin Orders Page (Part01 - using persian datepicker)

--- EShop.RazorPage/Models/Orders/OrderFilterData.cs
+++ EShop.RazorPage/Models/Orders/OrderFilterData.cs
@@ -4,7 +4,7 @@ public class OrderFilterData : BaseDto
     public long UserId { get; set; }
     public string UserFullName { get; set; }
     public OrderStatus Status { get; set; }
-    public string? Shire { get; set; }
+    public string? State { get; set; }
     public string? City { get; set; }
     public string? ShippingType { get; set; }
     public int TotalPrice { get; set; }
---
+++ EShop.RazorPage/Pages/Admin/Orders/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using EShop.RazorPage.Infrastructure.RazorUtils;
+using EShop.RazorPage.Infrastructure.Utils;
+using EShop.RazorPage.Models.Orders;
+using EShop.RazorPage.Services.Orders;
+
+namespace EShop.RazorPage.Pages.Admin.Orders;
+
+public class IndexModel : BaseRazorFilter<OrderFilterParams>
+{
+    private IOrderService _orderService;
+
+    public IndexModel(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    public OrderFilterResult FilterResult { get; set; }
+    public async Task OnGet(string? startDate, string? endDate)
+    {
+        if (string.IsNullOrWhiteSpace(startDate) == false)
+            FilterParams.StartDate = startDate.ToMiladi();
+
+
+        if (string.IsNullOrWhiteSpace(endDate) == false)
+            FilterParams.StartDate = endDate.ToMiladi();
+
+        FilterResult = await _orderService.GetOrders(FilterParams);
+    }
+}
+
